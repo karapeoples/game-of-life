5105763b4089978bcc05367f9dcf3afec74025a9
karapeoples <kararpeoples@yahoo.com>
2020-10-16 16:07:05 -0500

    All complete with Notes

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# game-of-life
+# GAME-of-LIFE
 CS Build Week Conway's Game of Life in React
+(Kara R. Peoples October 2020)
+
+# Rules of Conway's Game of Life
+
+### Death
+..* Underpopulation ~ If a Cell has two dead neighbors it dies from neglect. Who said social distancing is a good thing?
+..* Overpoplutaion ~ If a Cell has more than three live neighbors it dies from crowding. Crowds are Dangerous too!
+
+### Survivability
+
+..* Existence ~ If a Cell has 3 live neighbors it survives until it end up in overpopulation or underpopulation but allows for instances of reproduction. Just like the porridge these conditions are just right!!
+..*Reproduction ~ Any Dead Cell comes to Life if Existence is Correct and it has Three Live Neighbors. Just like that the relatives dies the next time round....
+
+
+# Cellular Automata
+
+> Cellular Automaton (plural Celular Autmata) A way to show cell death and life in a 3d manor on a 2d Graph. Also an recursive algorithm that implements these ideas. As seen in my quotes above seeing the rules help us compare real life in any type of population how it would work due to the rules.
+
+
+# Turing Completeness' of Conway's "Game of Life"
+
+> Turing Completeness involves the planning of the running of an algorithm. It allows us to write code that will compare boxes according to the rules of the Game of Life. Implemented in Javascript in an if else type of statement to compare all boxes to others on a 2d graph.
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,8 @@
 import React, { useState, useCallback, useRef } from 'react'
+//!Introduction to a Package that use a Function produce to act as a double buffer by allowing you to return unmutable state and do mutations to a copy of state rather than actual state.
 import produce from 'immer'
 
-
+//$Check Neighbors by location of square
 const operations = [
 	[0, 1],
 	[1, 1],
@@ -12,124 +13,134 @@ const operations = [
 	[1, 0],
 	[-1, 0],
 ]
+//$Starting Variables for Resting Grid Size and Speed
 let numRows = 15
 let numCols = 15
 let numSecs = 100
+
+//%Component Start
 const Grid = () => {
-
-
-  const [gridBox, setGridBox] = useState(Array.from({ length: numRows }).map(() => Array.from({ length: numCols }).fill(0)))
-  const [running, setRunning] = useState(false)
+	//#State to Make all Cells at Resting Dead 0=Cell Dead change to 1=Cell Alive Also an Array Data Structure
+	const [gridBox, setGridBox] = useState(Array.from({ length: numRows }).map(() => Array.from({ length: numCols }).fill(0)))
+	//#State to Toggle Recursive Algorithm On and Off
+	const [running, setRunning] = useState(false)
+	//#State to Keep Track of Generations Produced
   const [generations, setGenerations] = useState(0)
 
-
+//%Helper Functions
+	//#useRef hook allows us to reference recursive state
   const runningRef = useRef(running)
-  runningRef.current = running
-
-  const runSimulation = useCallback(() => {
-    if (!runningRef.current) {
-      return
-    }
-
-    setGridBox((g) => {
-      return produce(g, (gridCopy) => {
-        for (let i = 0; i < numRows; i++) {
-          for (let k = 0; k < numCols; k++) {
-            let neighbors = 0
-            operations.forEach(([x, y]) => {
-              const newI = i + x
-              const newK = k + y
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
-                neighbors += g[newI][newK]
-              }
-            })
-
-            if (neighbors < 2 || neighbors > 3) {
-              gridCopy[i][k] = 0
-            } else if (g[i][k] === 0 && neighbors === 3) {
-              gridCopy[i][k] = 1
-
-            }
-          }
-        }
-      })
-    })
-
-    setTimeout(runSimulation, numSecs,setGenerations((prevCount) => prevCount + 1))
-  }, [])
-
-  const ten = () => {
-    numRows = 10
+	runningRef.current = running
+	//#GRID SIZE HELPERS Variable Reassignment
+	const ten = () => {
+		numRows = 10
 		numCols = 10
 		setGridBox(Array.from({ length: numRows }).map(() => Array.from({ length: numCols }).fill(0)))
-}
-const fifty = () => {
-  numRows = 50
-  numCols = 50
-  setGridBox(Array.from({ length: numRows }).map(() => Array.from({ length: numCols }).fill(0)))
-}
-
-  const quarter = () => {
-    numRows = 25
+		}
+		const quarter = () => {
+		numRows = 25
 		numCols = 25
 		setGridBox(Array.from({ length: numRows }).map(() => Array.from({ length: numCols }).fill(0)))
-  }
-const seed = () => {
-    setGridBox((g) => {
-      return produce(g, (gridCopy) => {
-        for (let i = 0; i < numRows; i++) {
-          for (let k = 0; k < numCols; k++) {
-            gridCopy[i][k] = Math.floor(Math.random() * 2)
-          }
-          setGenerations(0);
-          setRunning(false)
-        }
-      })
-    })
-}
-  const fast = () => {
-    numSecs = 25
-    setRunning(!running)
+	}
+	const fifty = () => {
+		numRows = 50
+		numCols = 50
+		setGridBox(Array.from({ length: numRows }).map(() => Array.from({ length: numCols }).fill(0)))
+	}
+
+	//#Random Dead and Alive Cells
+	const seed = () => {
+		setGridBox((g) => {
+			return produce(g, (gridCopy) => {
+				for (let i = 0; i < numRows; i++) {
+					for (let k = 0; k < numCols; k++) {
+						gridCopy[i][k] = Math.floor(Math.random() * 2)
+					}
+					setGenerations(0)
+					setRunning(false)
+				}
+			})
+		})
+	}
+	//#Speed Variable Reassignment Functions and Algorithm Toggled to start main function
+	const fast = () => {
+		numSecs = 25
+		setRunning(!running)
 		if (!running) {
 			runningRef.current = true
 			runSimulation()
 		}
-  }
-   const slow = () => {
-				numSecs = 1000
-				setRunning(!running)
-				if (!running) {
-					runningRef.current = true
-					runSimulation()
-				}
-	 }
-
+	}
+	const slow = () => {
+		numSecs = 1000
+		setRunning(!running)
+		if (!running) {
+			runningRef.current = true
+			runSimulation()
+		}
+	}
+	//#Pop Ups for the Rules and Information
 	const about = () => {
 		alert(`Who is John Conway?
-	John Horton Conway(12/26/1937 - 04/11/2020) an English mathematician.
-	He was was active in a multitude of different theories, such as, finite groups, knot theory, number theory,
-	combinatorial game theory and coding theory.
-	However, he was mostly known for his invention of the cellular automaton known as "The Game of Life"`)
+		John Horton Conway(12/26/1937 - 04/11/2020) an English mathematician.
+		He was was active in a multitude of different theories, such as, finite groups, knot theory, number theory,
+		combinatorial game theory and coding theory.
+		However, he was mostly known for his invention of the cellular automaton known as "The Game of Life"`)
 	}
 
 	const aboutGame = () => {
 		alert(`What is "The Game of Life?"
-	The game of life is a cellular automaton ( A collection of "colored" cells on a grid of specified shape that
-	evolves through a number of discrete time steps according to a set of rules based on the states of
-	neighboring cells.)
-How can Conway's "Game of Life" be useful in real life?
-	It can help visualize how human or animal groups themselves rotate through environments or migrate through them in
-	search of more resources.`)
+		The game of life is a cellular automaton ( A collection of "colored" cells on a grid of specified shape that
+		evolves through a number of discrete time steps according to a set of rules based on the states of
+		neighboring cells.)
+	How can Conway's "Game of Life" be useful in real life?
+		It can help visualize how human or animal groups themselves rotate through environments or migrate through them in
+		search of more resources.`)
 	}
 
 	const rules = () => {
 		alert(`The rules of "The Game of Life":
-		#1- Any live cell with fewer than two live neighbors dies (under-population).
-		#2- Any live cell with or three live neighbors live on to the next generation.
-		#3- Any live cell with more than thee live neighbors dies (overpopulation).
-		#4- Any dead cell with exactly three live neighbors becomes a live cell (reproduction).`)
+			#1- Any live cell with fewer than two live neighbors dies (under-population).
+			#2- Any live cell with three live neighbors live on to the next generation.
+			#3- Any live cell with more than three live neighbors dies (overpopulation).
+			#4- Any dead cell with exactly three live neighbors becomes a live cell (reproduction).`)
 	}
 
+//%Main Game Function
+	//#UseCallBack Hook Allowing this Algorithm to Run as a Callback Method and not be Overpowered by Data
+	const runSimulation = useCallback(() => {
+		if (!runningRef.current) {
+			return
+		}
+		//@ Game Logic and Rules Applied with an O (n) runtime because Grid Copy is either returning 1 or 0 against an array the operation/runtime would run n times against each cell for each generation making O(2n) simplifying to O(n)
+		setGridBox((g) => {
+			return produce(g,(gridCopy) => {
+				for (let i = 0; i < numRows; i++) {
+					for (let k = 0; k < numCols; k++) {
+						let neighbors = 0
+		//#For Each Coordinate in Operations Check Neighbors against Game Rule Logic and Switch Between Life and Death
+						// eslint-disable-next-line
+						operations.forEach(([x, y]) => {
+							const newI = i + x
+							const newK = k + y
+							if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+								neighbors += g[newI][newK]
+							}
+						})
+						if (neighbors < 2 || neighbors > 3) {
+							gridCopy[i][k] = 0
+						} else if (g[i][k] === 0 && neighbors === 3) {
+							gridCopy[i][k] = 1
+						}
+					}
+
+					}
+		})
+	})
+
+	//#Recursive Implmentation
+	setTimeout(runSimulation, numSecs, setGenerations((prevCount) => prevCount + 1))
+  }, [])
 
 
   return (
@@ -183,6 +194,7 @@ How can Conway's "Game of Life" be useful in real life?
 									style={{
 										width: 20,
 										height: 20,
+										/*Inline Styling to Show Cell Death and Life on Each Version of State */
 										background: gridBox[i][k] ? 'steelblue' : 'white',
                     border: '1px solid navy',
 									}}
